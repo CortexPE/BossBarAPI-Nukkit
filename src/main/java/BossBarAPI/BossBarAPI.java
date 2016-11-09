47c51cfaf927d546a16b7ad0f9f13c590c0af514
NycuRO <nghergu@yahoo.com>
2016-11-09 19:55:18 +0200

    Update BossBarAPI.java

--- src/main/java/BossBarAPI/BossBarAPI.java
+++ src/main/java/BossBarAPI/BossBarAPI.java
@@ -2,6 +2,16 @@ package BossBarAPI;
 
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.TextFormat;
+import cn.nukkit.network.protocol.BossEventPacket;
+
+import java.util.Map;
+
+import cn.nukkit.Server;
+import cn.nukkit.Player;
+
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerJoinEvent;
 
 public class BossBarAPI extends PluginBase {
 
@@ -19,4 +29,11 @@ public class BossBarAPI extends PluginBase {
     public void onDisable() {
         this.getLogger().info(TextFormat.DARK_RED + "BossBarAPI disabled!");
     }
+    
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) 
+        BossEventPacket pk = new BossEventPacket();
+        new TextFormat("Hi " + player.getName() + "!");
+        pk.eid = entity.getId();
+        this.getServer().broadcastPacket(entity.getViewers(), pk);
 }
