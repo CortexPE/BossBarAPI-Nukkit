5d2b518e7856b4c8083c6198b682fc52f73df8b9
NycuRO <nghergu@yahoo.com>
2016-11-09 19:26:46 +0200

    Update BossBarAPI.java

--- src/main/java/BossBarAPI/BossBarAPI.java
+++ src/main/java/BossBarAPI/BossBarAPI.java
@@ -7,14 +7,12 @@ public class BossBarAPI extends PluginBase {
 
     @Override
     public void onLoad() {
-        registerGenerator();
         this.getLogger().info(TextFormat.WHITE + "BossBarAPI loaded!");
     }
 
     @Override
     public void onEnable() {
         this.getLogger().info(TextFormat.DARK_GREEN + "BossBarAPI enabled!");
-        getServer().getPluginManager().registerEvents(listener, this);
     }
 
     @Override
