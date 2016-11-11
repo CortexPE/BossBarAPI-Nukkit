d61d5d08e94ad9c3d2f0bef0194d0a47c00837ad
NycuRO <nghergu@yahoo.com>
2016-11-11 14:11:39 +0200

    Update BossBarAPI.java

--- src/main/java/BossBarAPI/BossBarAPI.java
+++ src/main/java/BossBarAPI/BossBarAPI.java
@@ -10,11 +10,8 @@ import cn.nukkit.Server;
 import cn.nukkit.Player;
 import cn.nukkit.entity.Entity;
 
-import cn.nukkit.event.EventHandler;
-import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
 
-import cn.nukkit.event.Listener;
 import cn.nukkit.plugin.Plugin;
 
 public class BossBarAPI extends PluginBase {
