d12bbb2b896ba77bd7c821a4362b5d4772bb97af
NycuRO <nghergu@yahoo.com>
2016-11-12 09:22:39 +0200

    Update FakeAttribute.java

--- src/main/java/BossBarAPI/attributes/FakeAttribute.java
+++ src/main/java/BossBarAPI/attributes/FakeAttribute.java
@@ -1,4 +1,29 @@
 package BossBarAPI.attributes;
-
+// Translated from PiggyAuth, thx Piggy
 public class FakeAttribute {
-
+  
+  public int min, max, value, name;
+  
+  public FakeAttribute() {
+        super();
+  }
+  
+  public void getMinValue() {
+    return this.min;
+  }
+  
+  public void getMaxValue() {
+    return this.max;
+  }
+  
+  public void getValue() {
+    return this.value;
+  }
+  
+  public void getName() {
+    return this.name;
+  }
+  
+  public void getDefaultValue() {
+    return this.min;
+  }
