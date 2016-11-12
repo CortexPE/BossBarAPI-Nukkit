7cfdc7a4d58d4bf53669e95a0ed0a386a63223fc
NycuRO <nghergu@yahoo.com>
2016-11-12 09:17:34 +0200

    Update and rename src/main/java/BossBarAPI/BossBarAPITask.java to src/main/java/BossBarAPI/task/BossBarAPITask.java

---
+++ src/main/java/BossBarAPI/task/BossBarAPITask.java
@@ -0,0 +1,19 @@
+package BossBarAPI.task;
+
+import cn.nukkit.scheduler.PluginTask;
+import cn.nukkit.network.protocol.UpdateAttributesPacket;
+
+public class BossBarAPITask extends PluginTask<BossBarAPI> {
+
+    public BossBarAPITask(BossBarAPI owner) {
+        super(owner);
+    }
+
+    @Override
+    public void onRun(int currentTick) {
+        pk = new UpdateAttributesPacket();
+        pk.entries[] = new FakeAttribute(00, this.plugin.timeouttick[strtolower($player->getName())]) - 1, "minecraft:health");
+        pk.entityId = this.plugin.wither[strtolower(player.getName())].getId();
+        player.dataPacket(pk);
+    }
+}
